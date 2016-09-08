9d7f4e1c4f2692b13863257e75839bb48fad2952
root <75710541@qq.com>
2016-09-08 22:08:38 +0800

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,20 @@
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 . ../config.cfg
-#docker-compose -p repo up -d
-grep sry_yumrepo /etc/rc.local || echo "nohup python -m SimpleHTTPServer $CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &" >> /etc/rc.local 
-grep "SimpleHTTPServer $CONFIGSERVER_PORT" || nohup python -m SimpleHTTPServer $CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &
+
+
+rm -f yum_repo_readme.txt
+echo "please look yum_repo_readme.txt"
+echo "install repo command:"|tee -a yum_repo_readme.txt
+echo "curl -Ls http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/packages/centos/get_repo.sh|bash -s $CONFIGSERVER_IP:$CONFIGSERVER_PORT"|tee -a yum_repo_readme.txt
+echo "" | tee -a yum_repo_readme.txt 
+echo "yum install command:" | tee -a yum_repo_readme.txt
+echo "yum --disablerepo=\* --enablerepo=offlineshurenyunrepo install -y PACKAGE_NAME" | tee -a yum_repo_readme.txt
+echo "" | tee -a yum_repo_readme.txt
+echo "install docker-compose:" | tee -a yum_repo_readme.txt
+echo "curl -o /usr/local/bin/docker-compose http://$CONFIGSERVER_IP:$CONFIGSERVER_PORT/config/docker-compose-1.8.0/docker-compose && chmod +x /usr/local/bin/docker-compose"
+echo
+
+
+grep offline-yumrepo /etc/rc.local || echo "/bin/bash /data/sry3/offline-yumrepo/run.sh" >> /etc/rc.local
+ps aux|grep "SimpleHTTPServer $CONFIGSERVER_PORT"|grep -v grep|wc -l|grep 1 || nohup python -m SimpleHTTPServer $CONFIGSERVER_PORT 1>/tmp/sry_yumrepo.log 2>&1 &
--- yum_repo_readme.txt
+++ yum_repo_readme.txt
@@ -3,3 +3,5 @@ curl -Ls http://192.168.1.213:8081/packages/centos/get_repo.sh|bash -s 192.168.1
 
 yum install command:
 yum --disablerepo=\* --enablerepo=offlineshurenyunrepo install -y PACKAGE_NAME
+
+install docker-compose:
