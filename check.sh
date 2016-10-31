221347bfabab660e61737cc82d27a345ee30e293
root <75710541@qq.com>
2016-10-31 18:46:28 +0800

    update simplehttpserver

--- check.sh
+++ check.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+. ../config.cfg
+
 function check_port(){
 	name=$1
 	port=$2
-	netstat -lntp|grep $port && echo "$name is ok" || (echo "$name is error";exit 1)
+	netstat -lntp|grep $port &>/dev/null  && echo "$name is ok" || (echo "$name is error";exit 1)
 }
 
-check_port logstash 5011
-check_port kibana 5601
-curl http://127.0.0.1:9200/_cluster/health 2>/dev/null|python -m json.tool|grep 'status.*green' && echo "elasticsearch is ok" || (echo "elasticsearch is error"; exit 1)
-
-
-
-
+check_port yumrepo $CONFIGSERVER_PORT
 
--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if  ps aux|grep "MultithreadedSimpleHTTPServer" ;then
-	ps aux|grep "MultithreadedSimpleHTTPServer"|grep -v grep|awk '{print $2}'|xargs kill
+if  ps aux|grep "ForkStaticServer" ;then
+	ps aux|grep "ForkStaticServer"|grep -v grep|awk '{print $2}'|xargs kill
 fi
 
