a96c1e1c1c0f3a378ac827bdd802a575ef136459
root <75710541@qq.com>
2016-10-31 18:46:55 +0800

    update simplehttpserver

---
+++ ForkStaticServer.py
@@ -0,0 +1,27 @@
+#!/bin/env python
+
+import socket
+import SocketServer
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+
+class ForkingHTTPServer(SocketServer.ForkingTCPServer):
+
+   allow_reuse_address = 1
+
+   def server_bind(self):
+       """Override server_bind to store the server name."""
+       SocketServer.TCPServer.server_bind(self)
+       host, port = self.socket.getsockname()[:2]
+       self.server_name = socket.getfqdn(host)
+       self.server_port = port
+
+
+def main(HandlerClass=SimpleHTTPRequestHandler,
+        ServerClass=ForkingHTTPServer):
+   BaseHTTPServer.test(HandlerClass, ServerClass)
+
+
+if __name__ == '__main__':
+   main()
