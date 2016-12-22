167cf43978d4fc34d0ed674a5aae2ce0e5876611
root <75710541@qq.com>
2016-12-22 11:47:37 +0800

    update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if  ps aux|grep "ForkStaticServer" ;then
+if  ps aux| grep -v grep | grep "ForkStaticServer" ;then
 	ps aux|grep "ForkStaticServer"|grep -v grep|awk '{print $2}'|xargs kill
 fi
 
